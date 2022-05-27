89b1e9c5937ec699dde09e574b315a964afb6219
EslebanPerez <eslebanperez@gmail.com>
2022-05-27 16:39:02 -0500

    Reasignando componentes en un modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { contadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+import { HeroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    contadorComponent,
-    HeroeComponent,
-    ListadoComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
